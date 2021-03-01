f102e287efe87d57230fc0938410f75d686bae88
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2021-03-01 17:24:25 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -9,7 +9,8 @@ def load_gnomad_exome(data_folder):
     assert os.path.exists(infile)
     results = {}
     with open(infile) as fp:
-        header = fp.next
+        csv_reader = reader(fp)
+        header = csv_reader.next
         for line in fp:
             rec = dict(zip(header,line))
             var = rec["release"] + "_" + str(rec["chromosome"]) + "_" + str(rec["position"]) + "_" + rec["reference"] + "_" + rec["alternative"]       

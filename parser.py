bb39ecc741b41d64a98c3173b1172c4479230a8a
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2021-03-01 17:21:54 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -7,15 +7,18 @@ logging = config.logger
 def load_gnomad_exome(data_folder):
     infile = os.path.abspath("/opt/biothings/GRCh37/gnomAD_exomes/r2.1/GnomadExomes.tsv")
     assert os.path.exists(infile)
-    dat = pandas.read_csv(infile,sep="\t",squeeze=True,quoting=csv.QUOTE_NONE).to_dict(orient='records')
     results = {}
-    for rec in dat:
-        var = rec["release"] + "_" + str(rec["chromosome"]) + "_" + str(rec["position"]) + "_" + rec["reference"] + "_" + rec["alternative"]       
-        _id = hashlib.sha224(var.encode('ascii')).hexdigest()       
-        process_key = lambda k: k.replace(" ","_").lower()
-        rec = dict_convert(rec,keyfn=process_key)
-        rec = dict_sweep(rec,vals=[np.nan])
-        results.setdefault(_id,[]).append(rec)
-    for _id,docs in results.items():
-        doc = {"_id": _id, "gnomad_exome" : docs}
-        yield doc
+    with open(infile) as fp:
+        header = fp.next
+        for line in fp:
+            rec = dict(zip(header,line))
+            var = rec["release"] + "_" + str(rec["chromosome"]) + "_" + str(rec["position"]) + "_" + rec["reference"] + "_" + rec["alternative"]       
+            _id = hashlib.sha224(var.encode('ascii')).hexdigest()       
+            process_key = lambda k: k.replace(" ","_").lower()
+            rec = dict_convert(rec,keyfn=process_key)
+            rec = dict_sweep(rec,vals=[np.nan])
+            results.setdefault(_id,[]).append(rec)
+            doc = {"_id": _id, "gnomad_exome" : docs}
+            yield doc
+
+        

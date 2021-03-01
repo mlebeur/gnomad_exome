1181073fd45c36e9cc135e54ca86bfbe23a7ab7e
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2021-03-01 17:47:06 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -7,7 +7,6 @@ logging = config.logger
 def load_gnomad_exome(data_folder):
     infile = os.path.abspath("/opt/biothings/GRCh37/gnomAD_exomes/r2.1/GnomadExomes.tsv")
     assert os.path.exists(infile)
-    results = {}
     with open(infile) as fp:
         reader = csv.reader(fp)
         header = next(reader)
@@ -18,8 +17,7 @@ def load_gnomad_exome(data_folder):
             process_key = lambda k: k.replace(" ","_").lower()
             rec = dict_convert(rec,keyfn=process_key)
             rec = dict_sweep(rec,vals=[np.nan])
-            results.setdefault(_id,[]).append(rec)
-            doc = {"_id": _id, "gnomad_exome" : docs}
+            doc = {"_id": _id, "gnomad_exome" : rec}
             yield doc
 
         
